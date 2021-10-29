e50ac1149d1f935fde6e420ee8d22b7caf7171f9
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2021-10-29 11:22:51 -0500

    Print request headers debugging

--- src/app.py
+++ src/app.py
@@ -52,6 +52,7 @@ def get_django_user():
             raise Exception("DJANGO_SESSION_COOKIE environment variable")
         session_id = request.cookies.get(DJANGO_SESSION_COOKIE)
         if not session_id:
+            print(request.headers)
             raise Exception("{cookie} cookie is missing".format(cookie=DJANGO_SESSION_COOKIE))
         api = "{django_login_api}/api/sessions_api/".format(
             django_login_api=DJANGO_LOGIN_API
